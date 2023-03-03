8feed5bcb2fb9c15d03d8d572287bc136e024b68
caldeira54 <caldeiraa54@gmail.com>
2023-03-03 17:23:03 -0300

    Adição de media query + Adição de novos campos no form

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,26 @@ function App() {
             <span>CEP</span>
             <input type='text' name='cep' className='input' placeholder='Digite o CEP' />
           </div>
+
+          <div className='field'>
+            <span>Rua</span>
+            <input type='text' name='rua' className='input' />
+          </div>
+
+          <div className='field'>
+            <span>Bairro</span>
+            <input type='text' name='bairro' className='input' />
+          </div>
+
+          <div className='field'>
+            <span>Cidade</span>
+            <input type='text' name='cidade' className='input' />
+          </div>
+
+          <div className='field'>
+            <span>Estado</span>
+            <input type='text' name='estado' className='input' />
+          </div>
         </form>
       </main>
 
